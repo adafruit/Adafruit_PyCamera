e824c11b6ee07b7a728ae6bd7d8234a502e70876
Liz <liz@adafruit.com>
2024-02-20 14:36:50 -0500

    clang and doxy

--- Adafruit_PyCamera.cpp
+++ Adafruit_PyCamera.cpp
@@ -684,10 +684,16 @@ void Adafruit_PyCamera::blitFrame(void) {
   esp_camera_fb_return(frame);
 }
 
-void Adafruit_PyCamera::refresh(void) {
-
-  esp_camera_fb_return(frame);
-}
+/**************************************************************************/
+/**
+ * @brief Blits the current frame buffer to the display.
+ *
+ * @details This function returns the frame buffer to the camera for
+ * reuse. Used when you are drawing your own bitmap to the framebuffer
+ * in user code.
+ */
+/**************************************************************************/
+void Adafruit_PyCamera::refresh(void) { esp_camera_fb_return(frame); }
 
 /**************************************************************************/
 /**
