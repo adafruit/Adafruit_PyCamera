80a501f9d4dc633472f3ab4bc31dea128beb3ec0
Doxygen CI <ci-arduino@invalid>
2024-02-20 19:52:51 +0000

    Deploy docs to GitHub Pages from commit 2cc8d20afe
    Commit: 2cc8d20afe9d210cb62753fb15c316152a39d71d
    GitHub Actions run: 7979133577

--- html/search/functions_9.js
+++ html/search/functions_9.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['readacceldata',['readAccelData',['../class_adafruit___py_camera.html#a66a887c25e59d13fc11166b37801e993',1,'Adafruit_PyCamera::readAccelData(int16_t *x, int16_t *y, int16_t *z)'],['../class_adafruit___py_camera.html#a53f58b32b10ae4dd5bd0d48eadf63a58',1,'Adafruit_PyCamera::readAccelData(float *x, float *y, float *z)']]],
   ['readbatteryvoltage',['readBatteryVoltage',['../class_adafruit___py_camera.html#a1908a1f1ddd0e4e82a00183edd05964f',1,'Adafruit_PyCamera']]],
-  ['readbuttons',['readButtons',['../class_adafruit___py_camera.html#aad869fd580f8d0aa0fd20e38a2748f69',1,'Adafruit_PyCamera']]]
+  ['readbuttons',['readButtons',['../class_adafruit___py_camera.html#aad869fd580f8d0aa0fd20e38a2748f69',1,'Adafruit_PyCamera']]],
+  ['refresh',['refresh',['../class_adafruit___py_camera.html#ac4e7c68209d65ecccf2618715a91c630',1,'Adafruit_PyCamera']]]
 ];
